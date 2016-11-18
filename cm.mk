945b308dc1e6782c909761c8abb8cf6c9fa86683
karthick111 <karthickrko61@gmail.com>
2016-11-18 10:48:08 +0100

    cleanup device tree
    Signed-off-by: karthick111 <karthickrko61@gmail.com>

--- cm.mk
+++ cm.mk
@@ -2,7 +2,7 @@
 PRODUCT_RELEASE_NAME := vivaltods5m
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/vivaltods5m/device_vivaltods5m.mk)
